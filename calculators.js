c1ae751c2fa89cbef299a018c0e5e2af3e0d6b53
chsahib000786 <masiad530@gmail.com>
2026-09-26 20:52:40 +0500

    Update calculators.js

--- calculators.js
+++ calculators.js
@@ -3,5 +3,10 @@ const calculators = [
     name: "Percentage Calculator",
     file: "percentage-calculator.html",
     description: "Calculate percentages quickly and easily."
+  },
+  {
+    name: "General Calculator",
+    file: "calculator.html",
+    description: "Simple and scientific general calculator."
   }
 ];
